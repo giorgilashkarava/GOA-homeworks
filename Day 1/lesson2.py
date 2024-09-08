dcc8f39952606b54a5e74a0ffb31221290a5e3cd
giorgilashkarava <giolashqarava58@gmail.com>
2024-09-08 14:31:26 +0400

    first commit

---
+++ Day 1/lesson2.py
@@ -0,0 +1,34 @@
+name="giorgi"
+#name არის ცვლადი
+#= არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi" არის ცვლადისთვის მნისვნელობა
+
+surname="lashkarava"
+
+#print(name)
+#print ფუნქციის გადაცემა ეკრანზე გამოსატანი ობიეცტი
+
+
+print("name")
+
+
+print("giorgi")
+
+name= "giorgi"
+age=13 #ეს არის int (integer) მტელი რიცხვი
+height=159.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boollean (bool) ტიპის ცვლადი 
+
+knows_programming=True  #true ან false
+is_ugly=False
+
+isugly=False #ჯავასქრიპტული camelcase
+
+
+print(name +  "" + surname)
+
+print(name + "" + str(age))
+
+print(type(age))
+print(type(name))
+
