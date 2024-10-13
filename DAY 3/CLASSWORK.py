9b7ff64fa7fbb7f7f78ad08a365a7263a8c3d0db
giorgilashkarava <giolashqarava58@gmail.com>
2024-10-14 00:05:12 +0400

    second commit

---
+++ DAY 3/CLASSWORK.py
@@ -0,0 +1,24 @@
+
+age=13
+
+age_in_5_years=13+5
+
+print(age_in_5_years)
+
+number=10
+
+number2=12
+
+print(number + number2)
+
+mother_age=36
+
+print(mother_age) 
+
+print(mother_age * age)
+
+name="Giorgi"
+print(name)
+name="ilia"
+print(name)
+
