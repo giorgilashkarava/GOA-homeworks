e63831ea9db0d887f28c82d18e3642a5934087d2
giorgilashkarava <giolashqarava58@gmail.com>
2025-02-25 22:29:29 +0400

    goa

--- level 07/homework/homework.py
+++
@@ -1,6 +0,0 @@
-mom_age=37
-dad_age=45
-num_1=25
-print(mom_age+num_1)
-print(dad_age+num_1)
-
---
+++ level 49/homework/homework.py
@@ -0,0 +1,8 @@
+#1
+def derive(coefficient, exponent):
+    nex = 0
+    if exponent == 2:
+        nex = 2
+    else:
+        nex = exponent - 1
+    return str( str(coefficient*exponent) + "x^" + str(nex)
